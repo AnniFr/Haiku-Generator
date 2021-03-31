a520ae5aa99db205a99a5cd8905472c56c630023
Iain Ross <iainpatrickross@gmail.com>
2021-03-31 22:06:31 +0100

    experiments with syntax trees and control flow

---
+++ afternoonExperiments.js
@@ -0,0 +1,696 @@
+const futureVerbs = [{
+    word: 'run',
+    syllables: 1
+},
+{
+    word: 'jump',
+    syllables: 1
+},
+{
+    word: 'fly',
+    syllables: 1
+},
+{
+    word: 'make',
+    syllables: 1
+},
+{
+    word: 'dance',
+    syllables: 1
+},
+{
+    word: 'call',
+    syllables: 1
+},
+{
+    word: 'do',
+    syllables: 1
+},
+{
+    word: 'code',
+    syllables: 1
+},
+{
+    word: 'smile',
+    syllables: 1
+},
+{
+    word: 'be',
+    syllables: 1
+},
+{
+    word: 'forget',
+    syllables: 2
+},
+{
+    word: 'clean',
+    syllables: 1
+},
+{
+    word: 'begin',
+    syllables: 2
+},
+{
+    word: 'draw',
+    syllables: 1
+},
+{
+    word: 'program',
+    syllables: 2
+},
+{
+    word: 'become',
+    syllables: 2
+},
+{
+    word: 'concentrate',
+    syllables: 3
+},
+{
+    word: 'pretend',
+    syllables: 2
+},
+{
+    word: 'imagine',
+    syllables: 3
+},
+];
+
+const presentVerbs = [
+{
+    word: 'run',
+    syllables: 1
+},
+{
+    word: 'running',
+    syllables: 2
+},
+{
+    word: 'jumping',
+    syllables: 2
+},
+{
+    word: 'flying',
+    syllables: 2
+},
+{
+    word: 'making',
+    syllables: 2
+},
+{
+    word: 'dancing',
+    syllables: 2
+},
+{
+    word: 'calling',
+    syllables: 2
+},
+{
+    word: 'doing',
+    syllables: 2
+},
+{
+    word: 'coding',
+    syllables: 2
+},
+{
+    word: 'smiling',
+    syllables: 2
+},
+{
+    word: 'being',
+    syllables: 2
+},
+{
+    word: 'forgetting',
+    syllables: 3
+},
+{
+    word: 'cleaning',
+    syllables: 2
+},
+{
+    word: 'beginning',
+    syllables: 3
+},
+{
+    word: 'drawing',
+    syllables: 2
+},
+{
+    word: 'programming',
+    syllables: 3
+},
+{
+    word: 'becoming',
+    syllables: 3
+},
+{
+    word: 'concentrating',
+    syllables: 4
+},
+{
+    word: 'pretending',
+    syllables: 3
+},
+{
+    word: 'imagining',
+    syllables: 4
+},
+];
+
+const nouns = [{
+    word: 'tree',
+    syllables: 1
+},
+{
+    word: 'cat',
+    syllables: 1
+},
+{
+    word: 'dog',
+    syllables: 1
+},
+{
+    word: 'bear',
+    syllables: 1
+},
+{
+    word: 'frog',
+    syllables: 1
+},
+{
+    word: 'jar',
+    syllables: 1
+},
+{
+    word: 'eye',
+    syllables: 1
+},
+{
+    word: 'bird',
+    syllables: 1
+},
+{
+    word: 'spoon',
+    syllables: 1
+},
+{
+    word: 'paper',
+    syllables: 2
+},
+{
+    word: 'rabbit',
+    syllables: 2
+},
+{
+    word: 'keyboard',
+    syllables: 2
+},
+{
+    word: 'iron',
+    syllables: 2
+},
+{
+    word: 'flower',
+    syllables: 2
+},
+{
+    word: 'garden',
+    syllables: 2
+},
+{
+    word: 'window',
+    syllables: 2
+},
+{
+    word: 'water',
+    syllables: 2
+},
+{
+    word: 'fire',
+    syllables: 1
+},
+{
+    word: 'bubble',
+    syllables: 2
+},
+{
+    word: 'knowledge',
+    syllables: 2
+},
+{
+    word: 'hamster',
+    syllables: 2
+},
+{
+    word: 'fantasy',
+    syllables: 3
+},
+{
+    word: 'computer',
+    syllables: 3
+},
+{
+    word: 'animal',
+    syllables: 3
+},
+{
+    word: 'umbrella',
+    syllables: 3
+},
+];
+
+
+const adjectives = [{
+    word: 'red',
+    syllables: 1
+},
+{
+    word: 'blue',
+    syllables: 1
+},
+{
+    word: 'wet',
+    syllables: 1
+},
+{
+    word: 'dry',
+    syllables: 1
+},
+{
+    word: 'cold',
+    syllables: 1
+},
+{
+    word: 'hot',
+    syllables: 1
+},
+{
+    word: 'fat',
+    syllables: 1
+},
+{
+    word: 'thin',
+    syllables: 1
+},
+{
+    word: 'little',
+    syllables: 1
+},
+{
+    word: 'big',
+    syllables: 1
+},
+{
+    word: 'short',
+    syllables: 1
+},
+{
+    word: 'plain',
+    syllables: 1
+},
+{
+    word: 'tall',
+    syllables: 1
+},
+{
+    word: 'proud',
+    syllables: 1
+},
+{
+    word: 'silver',
+    syllables: 2
+},
+{
+    word: 'lazy',
+    syllables: 2
+},
+{
+    word: 'crimson',
+    syllables: 2
+},
+{
+    word: 'golden',
+    syllables: 2
+},
+{
+    word: 'gentle',
+    syllables: 2
+},
+{
+    word: 'fierce',
+    syllables: 2
+},
+{
+    word: 'handsome',
+    syllables: 2
+},
+{
+    word: 'immense',
+    syllables: 2
+},
+{
+    word: 'ominous',
+    syllables: 3
+},
+{
+    word: 'emerald',
+    syllables: 3
+},
+{
+    word: 'elegant',
+    syllables: 3
+},
+{
+    word: 'glamorous',
+    syllables: 3
+},
+{
+    word: 'mysterious',
+    syllables: 4
+},
+];
+
+const adverbs = [{
+word: 'fast',
+syllables: 1
+},{
+    word: 'nicely',
+    syllables: 2
+},
+{
+    word: 'safely',
+    syllables: 2
+},
+{
+    word: 'nearly',
+    syllables: 2
+},
+{
+    word: 'rudely',
+    syllables: 2
+},
+{
+    word: 'kindly',
+    syllables: 2
+},
+{
+    word: 'oddly',
+    syllables: 2
+},
+{
+    word: 'seldom',
+    syllables: 2
+},
+{
+    word: 'never',
+    syllables: 2
+},
+{
+    word: 'truly',
+    syllables: 2
+},
+{
+    word: 'softly',
+    syllables: 2
+},
+{
+    word: 'quickly',
+    syllables: 2
+},
+{
+    word: 'inwardly',
+    syllables: 3
+},
+{
+    word: 'bitterly',
+    syllables: 3
+},
+{
+    word: 'rightfully',
+    syllables: 3
+},
+{
+    word: 'cleverly',
+    syllables: 3
+},
+{
+    word: 'youthfully',
+    syllables: 3
+},
+{
+    word: 'awkwardly',
+    syllables: 3
+},
+{
+    word: 'cheerfully',
+    syllables: 3
+},
+{
+    word: 'sheepishly',
+    syllables: 3
+},
+{
+    word: 'lovingly',
+    syllables: 3
+},
+{
+    word: 'aggresively',
+    syllables: 4
+},
+{
+    word: 'mysteriously',
+    syllables: 5
+},
+];
+
+const r = (arr) => {
+let index = Math.floor(Math.random() * arr.length);
+return arr[index];
+}
+//console.log(r(adverbs));
+
+const pGen = (percent) => {
+if (Math.floor(Math.random() * (100 / percent)) === 0) {
+    return true;
+} else {
+    return false;
+}
+}
+
+//line 1 generator
+
+//const nouns = [{word: 'aquifier', syllables: 4}, {word: "underwear", syllables: 3}, {word: "underwaterfish", syllables: 5}]
+
+
+const firstLineGen = () => {
+    const noun1 = r(nouns);
+    console.log("noun1: " + noun1.word)
+    if(noun1.syllables === 1) {
+        console.log("single syllable noun - we'll get there...")
+    }
+    //if first noun selected is five syllables return noun alone
+    if (noun1.syllables == 5) {
+        return noun1.word;
+    } else
+        //if first noun selected is 4 syllables select an article or 1 syllable adjective
+        if (noun1.syllables === 4) {
+            if (pGen(50) === true) {
+                let rPrep = Math.floor(Math.random) * 2;
+                const vowels = ["a", "e", "i", "o", "u"]
+                if (rPrep == 1) {
+                    rPrep = "the"
+                } else {
+                    if (vowels.includes(noun1.word[0])) {
+                        rPrep = "an";
+                    } else {
+                        rPrep = "a";
+                    }
+                }
+                return rPrep + " " + noun1.word
+            } else {
+                let adjective1 = r(adjectives);
+                while (adjective1.syllables > 1) {
+                    adjective1 = r(adjectives);
+                }
+                return adjective1.word + " " + noun1.word;
+            }
+        } else
+            // if first noun selected is 3 syllables
+            if (noun1.syllables === 3) {
+                //first choose whether an adjective will be selected
+                if (pGen(50) === true) {
+                    let adjective1 = r(adjectives);
+                    while (adjective1.syllables > 2) {
+                        adjective1 = r(adjectives);
+                    }
+                    console.log(adjective1);
+                    //and if it is check if it is 1 or two syllables
+                    if (adjective1.syllables === 2) {
+                        return adjective1.word + " " + noun1.word
+                    } else {
+                        //if its only one add an article before it
+                        let rPrep = Math.floor(Math.random) * 2;
+                        const vowels = ["a", "e", "i", "o", "u"]
+                        if (rPrep == 1) {
+                            rPrep = "the"
+                        } else {
+                            if (vowels.includes(adjective1.word[0])) {
+                                rPrep = "an";
+                            } else {
+                                rPrep = "a";
+                            }
+                        }
+                        return rPrep + " " + adjective1.word + " " + noun1.word;
+                    }
+                    //if we don't select an adjective we can add a 1 syllable verb on the end
+                } else {
+                    let verb1 = r(presentVerbs);
+                    while (verb1.syllables > 1) {
+                        verb1 = r(presentVerbs);
+                    }
+                    console.log(verb1);
+                    let rPrep = Math.floor(Math.random) * 2;
+                    const vowels = ["a", "e", "i", "o", "u"]
+                    if (rPrep == 1) {
+                        rPrep = "the"
+                    } else {
+                        if (vowels.includes(noun1.word[0])) {
+                            rPrep = "an";
+                        } else {
+                            rPrep = "a";
+                        }
+                    }
+                    return rPrep + " " + noun1.word + " " + verb1.word;
+                }
+            } else
+    //if selected noun is 2 syllables
+    if(noun1.syllables === 2) {
+            //again check if we will add an adjective
+            if (pGen(50) === true) {
+                let adjective1 = r(adjectives);
+                while (adjective1.syllables > 3) {
+                    adjective1 = r(adjectives);
+                }
+                console.log(adjective1);
+                //if we select an adjective that is 3 syllables return adjective and noun
+                if (adjective1.syllables === 3) {
+                    return adjective1.word + " " + noun1.word
+                } else {
+                    //if its less than 3 syllables choose if we will add a second adjective
+                    if (pGen(50) === true) {
+                        if (adjective1.syllables === 2) {
+                            let adjective2 = r(adjectives);
+                            while (adjective2.syllables !== 1) {
+                                adjective2 = r(adjectives);
+                            }
+                            console.log(adjective2);
+                                // if we do and its a 2 syllable adjective also return the sentence
+                                return adjective2.word + " " + adjective1.word + " " + noun1.word
+                            }
+                        else {
+                            //if its only 1 syllable 
+                            let adjective2 = r(adjectives);
+                            while (adjective2.syllables < 2) {
+                                adjective2 = r(adjectives);
+                            }
+                            console.log(adjective2)
+                            //if the first adjective was only 1 syllable but the 2nd is two return the sentence
+                            if (adjective2.syllables === 2) {
+                                return adjective2.word + " " + adjective1.word + " " + noun1.word;
+                            } else
+                                //if both adjectives are 1 syllable words decide if there will be an article
+                                if (pGen(50) === true) {
+                                    let rPrep = Math.floor(Math.random) * 2;
+                                    const vowels = ["a", "e", "i", "o", "u"]
+                                    if (rPrep == 1) {
+                                        rPrep = "the"
+                                    } else {
+                                        if (vowels.includes(adjective2.word[0])) {
+                                            rPrep = "an";
+                                        } else {
+                                            rPrep = "a";
+                                        }
+                                    }
+                                    return rPrep + " " + adjective2.word + " " + adjective1.word + " " + noun1.word;
+                                } else {
+                                    let verb1 = r(presentVerbs);
+                                    while (verb1.syllables > 1) {
+                                        verb1 = r(presentVerbs);
+                                    }
+                                    console.log(verb1);
+                                    return adjective2.word + " " + adjective1.word + " " + noun1.word + " " + verb1.word;
+                                }
+                        }
+                    }
+                }
+                } else {
+                    //decide if we will add an article
+                    if (pGen(50) === true) {
+                        console.log("article" + true)
+                        let rPrep = Math.floor(Math.random) * 2;
+                        const vowels = ["a", "e", "i", "o", "u"]
+                        if (rPrep == 1) {
+                            rPrep = "the"
+                        } else {
+                            if (vowels.includes(noun1.word[0])) {
+                                rPrep = "an";
+                            } else {
+                                rPrep = "a";
+                            }
+                        }
+                        //if so we will also add a verb on the end
+                        let verb1 = r(presentVerbs);
+                        while (verb1.syllables > 2) {
+                            verb1 = r(presentVerbs);
+                        }
+                        console.log(verb1);
+                        if (verb1.syllables == 2) {
+                            //if that verb is 2 syllables return sentence
+                            return rPrep + " " + noun1.word + " " + verb1.word;
+                        } else {
+                            //if it is only 1 add a 1 syllable adverb
+                            let adverb1 = r(adverbs);
+                            while (adverb1.syllables > 1) {
+                                adverb1 = r(adverbs);
+                            }
+                            console.log(adverb1);
+                            return rPrep + " " + noun1.word + " " + verb1.word + " " + adverb1.word
+                        }
+                    } else {
+                        //if we don't add an article we will add a verb at the end
+                        let verb1 = r(presentVerbs);
+                        while (verb1.syllables > 3) {
+                            verb1 = r(presentVerbs);
+                        }
+                        console.log(verb1);
+                        if (verb1.syllables == 3) {
+                            //if that verb is 3 syllables return sentence
+                            return noun1.word + " " + verb1.word;
+                        } else if (verb1.syllables == 2) {
+                            //if it is only 2 add a 1 syllable adverb
+                            let adverb1 = r(adverbs);
+                            while (adverb1.syllables != 1) {
+                                adverb1 = r(adverbs);
+                            }
+                            console.log(adverb1);
+                            return noun1.word + " " + verb1.word + " " + adverb1.word
+                        } else if (verb1.syllables == 1) {
+                            //if it is only 1 add a 2 syllable adverb and return
+                            let adverb1 = r(adverbs);
+                            while (adverb1.syllables != 2) {
+                                adverb1 = r(adverbs);
+                            }
+                            console.log(adverb1);
+                            return noun1.word + " " + verb1.word + " " + adverb1.word
+                    }
+                }
+
+            } 
+        }
+        
+    }
+
+
+            console.log(firstLineGen());
